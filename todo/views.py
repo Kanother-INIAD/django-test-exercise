0b05ce3dfcd4dd1eea0ce4e6a0c2ab309baef9b0
1F10230198 <s1f10231987@iniad.org>
2024-07-04 02:14:15 +0900

    add toppage

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.utils.timezone import make_aware
+from django.utils.dateparse import parse_datetime
+from todo.models import Task
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        task = Task(title=request.POST['title'],
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task.save()
+
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks' : tasks
+    }
+    return render(request, 'todo/index.html', context)
