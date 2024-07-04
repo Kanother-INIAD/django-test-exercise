fc72661f98ddb6becaf13e2acc48bcc78c28195d
1F10230198 <s1f10231987@iniad.org>
2024-07-04 13:46:54 +0900

    requirements.txtを修正2

--- todo/views.py
+++ todo/views.py
@@ -18,7 +18,5 @@ def index(request):
 
     tasks = Task.objects.all()
 
-    context = {
-        'tasks' : tasks
-    }
+    context = {'tasks': tasks}
     return render(request, 'todo/index.html', context)
