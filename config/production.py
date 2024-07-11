9b4b4af48a4a7ee416e62e6a30391b69c9b48af3
1F10230198 <s1f10231987@iniad.org>
2024-07-11 13:46:12 +0900

    correct deploy files

--- config/production.py
+++ config/production.py
@@ -6,11 +6,11 @@ DEBUG = True
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS.append(os.environ['RENDER_EXTERNAL_HOSTNAME'])
 
-MIDDLEWARE.insert(1, 'whitehouse.middleware.WhiteNoiseMiddleware')
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 DATABASES = {
-    'default' : dj_database_url.config()
+    'default': dj_database_url.config()
 }
 
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
