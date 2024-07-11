2f0e7ae5e243d674420721a8ff6adff820564a23
iniad <s1f102300379@iniad.org>
2024-07-11 14:21:32 +0900

    終了ページの追加

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
     path('<int:task_id>/delete', todo_views.delete, name='delete'),
+    path('<int:task_id>/close', todo_views.close, name='close'),
 ]
