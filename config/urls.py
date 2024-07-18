be0f8919877252f4c4412973c2f33fa99e4874f9
s1f10230179 <s1f102301796@iniad.org>
2024-07-18 14:18:21 +0900

    ブックマーク機能の追加

--- config/urls.py
+++ config/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:task_id>/update', todo_views.update, name='update'),
     path('<int:task_id>/delete', todo_views.delete, name='delete'),
     path('<int:task_id>/close', todo_views.close, name='close'),
+    path('<int:task_id>/bookmark/', todo_views.bookmark, name='bookmark'),
 ]
